f574aa4e11bf39dceb7ea8a6114809ff5b09fa7c
Sergryap <rs1180@mail.ru>
2023-03-08 16:30:25 +0500

    The code decomposition

--- main.py
+++ main.py
@@ -2,16 +2,13 @@ import subprocess
 import os
 
 
-def get_zip_archive(original_folder, archive_folder, file_name=None):
+def get_zip_archive(original_folder, archive_folder, file_archive_name=None):
 
-    if file_name:
+    if file_archive_name:
         archive_path = os.path.join(os.getcwd(), archive_folder)
-        file_path = os.path.join(archive_path, file_name)
+        file_path = os.path.join(archive_path, file_archive_name)
         os.makedirs(archive_folder, exist_ok=True)
-        process = subprocess.run(
-            ['zip', '-r', f'{file_path}', f'{original_folder}'],
-            capture_output=True,
-        )
+        process = subprocess.run(['zip', '-r', f'{file_path}', f'{original_folder}'])
         func_return = None
     else:
         process = subprocess.run(
@@ -26,28 +23,28 @@ def get_zip_archive(original_folder, archive_folder, file_name=None):
     return func_return
 
 
-def write_binary_archive_to_zip(binary_archive, file_name, archive_folder):
+def write_binary_archive_to_zip(binary_archive, file_archive_name, archive_folder):
     """Запись байтового архива в файл"""
 
     file_path = os.path.join(os.getcwd(), archive_folder)
     os.makedirs(file_path, exist_ok=True)
-    with open(os.path.join(file_path, file_name), 'wb') as binary_file:
+    with open(os.path.join(file_path, file_archive_name), 'wb') as binary_file:
         binary_file.write(binary_archive)
 
 
-def unpack_archive(file_name, unpack_folder, archive_folder):
+def unpack_archive(file_archive_name, unpack_folder, archive_folder):
     """
     Распаковка архива file_name в папку folder_name текущей директории
     из папки archive_folder
     """
 
     archive_path = os.path.join(os.getcwd(), archive_folder)
-    unpacking_file_path = os.path.join(archive_path, file_name)
+    unpacking_file_path = os.path.join(archive_path, file_archive_name)
     if os.path.isfile(unpacking_file_path):
         file_path = os.path.join(os.getcwd(), unpack_folder)
         os.makedirs(file_path, exist_ok=True)
         try:
-            process = subprocess.run(
+            subprocess.run(
                 ['unzip', f'{unpacking_file_path}', '-d', f'{file_path}'],
                 capture_output=True,
                 timeout=2
@@ -61,6 +58,6 @@ if __name__ == '__main__':
     ARCHIVE_FILE = 'archive.zip'
     UNPACK_FOLDER = 'unpack_archive'
     ORIGINAL_FOLDER = 'original_photo'
-    archive = get_zip_archive(ORIGINAL_FOLDER, ARCHIVE_FOLDER, file_name=None)
+    archive = get_zip_archive(ORIGINAL_FOLDER, ARCHIVE_FOLDER, file_archive_name=None)
     write_binary_archive_to_zip(archive, ARCHIVE_FILE, ARCHIVE_FOLDER)
     unpack_archive(ARCHIVE_FILE, UNPACK_FOLDER, ARCHIVE_FOLDER)
