5515c252760c23c33f234dc71cf05786cfed092e
Sergryap <rs1180@mail.ru>
2023-03-08 10:39:56 +0500

    The archive process has been modified

--- main.py
+++ main.py
@@ -1,15 +1,36 @@
 import subprocess
 import os
-import gzip
 
 
-def get_zip_archive(file_name, folder_name):
-    subprocess.run(['tar', '-cvf', f'{file_name}.tar.gz', f'{folder_name}'])
-    with open(f'{file_name}.tar.gz', 'rb') as file_in:
-        with gzip.open(f'{file_name}.zip', 'wb') as file_out:
-            file_out.writelines(file_in)
-    subprocess.run(['rm', f'{file_name}.tar.gz'])
+def get_zip_archive(file_name, folder_name, archive_folder):
+    archive_path = os.path.join(os.getcwd(), archive_folder)
+    file_path = os.path.join(archive_path, file_name)
+    os.makedirs(archive_folder, exist_ok=True)
+    process = subprocess.run(
+        ['zip', '-r', f'{file_path}', f'{folder_name}'],
+        capture_output=True,
+    )
+    if process.returncode:
+        raise ValueError('Archive process has problems, check dependencies!')
+
+
+def unpack_archive(file_name, folder_name, archive_folder):
+    """Распакаовка архива в папку folder_name текущей директории"""
+    archive_path = os.path.join(os.getcwd(), archive_folder)
+    unpacking_file_path = os.path.join(archive_path, file_name)
+    file_path = os.path.join(os.getcwd(), folder_name)
+    os.makedirs(file_path, exist_ok=True)
+    try:
+        process = subprocess.run(
+            ['unzip', f'{unpacking_file_path}', '-d', f'{file_path}'],
+            capture_output=True,
+            timeout=2
+        )
+    except subprocess.TimeoutExpired:
+        print('Архив уже распакован')
 
 
 if __name__ == '__main__':
-    get_zip_archive('photo', 'original_photo')
+    ARCHIVE_FOLDER = 'archives'
+    get_zip_archive('photo.zip', 'original_photo', ARCHIVE_FOLDER)
+    unpack_archive('photo.zip', 'unpack_archive', ARCHIVE_FOLDER)
